35061863be5b38c1e67a377018b2e2ed499dd45e
golonzovsky <alex.golonzovsky@gmail.com>
2022-10-10 11:27:13 +0200

    termenv for colors

--- color/color.go
+++ color/color.go
@@ -1,28 +1,25 @@
 package color
 
 import (
-	"fmt"
+	"github.com/muesli/termenv"
 )
 
-type Color int
+type Color termenv.Color
 
-const escape = "\x1b"
+var (
+	p = termenv.ColorProfile()
 
-const (
-	Black Color = iota + 30
-	Red
-	Green
-	Yellow
-	Blue
-	Magenta
-	Cyan
-	White
+	Black   = p.Color("#000000")
+	Gray    = p.Color("#546a7b")
+	Red     = p.Color("#ff0054")
+	Green   = p.Color("#73f59f")
+	Yellow  = p.Color("#ffbd00")
+	Blue    = p.Color("#71BEF2")
+	Magenta = p.Color("#9d4edd")
+	Cyan    = p.Color("#66C2CD")
+	White   = p.Color("#B9BFCA")
 )
-
-func (c Color) sequence() int {
-	return int(c)
-}
 
 func Apply(val string, c Color) string {
-	return fmt.Sprintf("%s[%dm%s%s[0m", escape, c.sequence(), val, escape)
+	return termenv.String(val).Foreground(c).String()
 }
