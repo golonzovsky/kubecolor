bce6e4c8ab5f977410c4f9dd62274da19ff04668
golonzovsky <alex.golonzovsky@gmail.com>
2022-10-12 22:03:47 +0200

    minor color adjustments

--- color/color.go
+++ color/color.go
@@ -9,14 +9,14 @@ type Color termenv.Color
 var (
 	p = termenv.ColorProfile()
 
-	// https://coolors.co/ff0054-ffc31f-73f59f-66c2cd-32a1ec-546a7b-9d4edd-7b2cbf-e0aaff-b9bfca
+	// https://coolors.co/ff0054-ffc31f-73f59f-57b2ef-1f99ea-546a7b-9d4edd-7b2cbf-e0aaff-b9bfca
 
 	Red          = p.Color("#ff0054")
 	Yellow       = p.Color("#ffc31f")
 	Green        = p.Color("#73f59f")
 	Cyan         = p.Color("#66C2cd")
-	BlueLight    = p.Color("#32A1EC")
-	Blue         = p.Color("#3A86FF")
+	BlueLight    = p.Color("#57B2EF")
+	Blue         = p.Color("#1F99EA")
 	Gray         = p.Color("#546a7b")
 	GrayLight    = p.Color("#b9bfca")
 	Magenta      = p.Color("#9d4edd")
